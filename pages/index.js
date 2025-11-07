54bfca2c2a5e8e346f6e29819be45e74a694e52e
GILMAR ALVES DE OLIVEIRA <GILALVES.OLIVEIRA@OUTLOOK.COM>
2025-11-07 11:13:06 -0300

    Tela Home

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,15 @@
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import styles from '../styles/Home.module.css'
+import { useState } from 'react'
 
 export default function Home() {
+  const [centerImage, setCenterImage] = useState('roupas.jpeg'); // Estado para a imagem central
+
+  const handleClick = (imageName) => {
+    setCenterImage(imageName); // Muda a imagem central ao clicar
+  }
+
   return (
     <div className={styles.container}>
       <Header />
@@ -11,9 +18,41 @@ export default function Home() {
         <p className={styles.description}>
           Encontre as joias de prata mais exclusivas e elegantes para você.
         </p>
-        <button className={styles.shopButton}>Comece a Comprar</button>
       </main>
-      <Footer /> {/* Incluindo o Footer aqui */}
+
+      <div className={styles.imageContainer}>
+        <div className={styles.imageWrapper}>
+          <img
+            src="/imagens/roupas.jpeg"
+            alt="Roupas"
+            className={centerImage === 'roupas.jpeg' ? styles.centerImage : styles.sideImage}
+            onClick={() => handleClick('roupas.jpeg')}
+          />
+          <div className={styles.imageText}>Roupas</div>
+        </div>
+        
+        <div className={styles.imageWrapper}>
+          <img
+            src="/imagens/prata.jpeg"
+            alt="Prata"
+            className={centerImage === 'prata.jpeg' ? styles.centerImage : styles.sideImage}
+            onClick={() => handleClick('prata.jpeg')}
+          />
+          <div className={styles.imageText}>Joias</div>
+        </div>
+        
+        <div className={styles.imageWrapper}>
+          <img
+            src="/imagens/roupas_joias.jpeg"
+            alt="Roupas + Joias"
+            className={centerImage === 'roupas_joias.jpeg' ? styles.centerImage : styles.sideImage}
+            onClick={() => handleClick('roupas_joias.jpeg')}
+          />
+          <div className={styles.imageText}>Roupas + Joias</div>
+        </div>
+      </div>
+
+      <Footer />
     </div>
   )
 }
